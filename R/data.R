6b244e32f63dd3ba4c02b577f355f99f634dcd86
LCHansson <love.hansson@gmail.com>
2020-04-18 14:28:00 +0200

    More coherent API for values

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Compose a query to fetch metadata from the Kolada API. Its use is mainly
 #'
-#' Mainly used as a supporting function for \code{get_kld_data()} but can also be
+#' Mainly used as a supporting function for \code{link{get_values}} but can also be
 #' used to create a working URL to paste in your web browser.
 #'
 #' @param kpi What kpis should be fetched? Can be a single name or a vector of
@@ -60,7 +60,7 @@ compose_data_query <- function(kpi = NULL, municipality = NULL, period = NULL, o
 #' @param simplify Whether to make results more human readable.
 #'
 #' @export
-get_kld_data <- function(kpi = NULL, municipality = NULL, period = NULL, ou = NULL, simplify = TRUE) {
+get_values <- function(kpi = NULL, municipality = NULL, period = NULL, ou = NULL, simplify = TRUE) {
   query <- compose_data_query(kpi, municipality, period, ou)
 
   res <- httr::GET(query)
@@ -77,10 +77,45 @@ get_kld_data <- function(kpi = NULL, municipality = NULL, period = NULL, ou = NU
       # Convert codes to names
       dplyr::mutate(
         municipality_id = municipality,
-        municipality = municipality_id_to_name(municipality_get(), municipality)
+        municipality = municipality_id_to_name(get_municipality(), municipality)
       ) %>%
       dplyr::rename(year = period)
   }
 
   ret
+}
+
+#' Simplify a Kolada value table
+#'
+#'
+#'
+#' @param values_df A Kolada value table, as created by \code{\link{get_values}}.
+#'
+#' @export
+
+values_minimize <- function(values_df) {
+  mins <- values_df %>%
+    purrr::map(dplyr::n_distinct)
+
+  drop <- names(mins[mins == 1])
+
+  values_df %>%
+    dplyr::select_if(names(.) %in% c("kpi", "municipality", "value") | purrr::map(., dplyr::n_distinct) > 1)
+}
+
+#' Create KPI long-form descriptions to add to a plot
+#'
+#'
+#'
+#' @param values_df A Kolada value table, as created by \code{\link{get_values}}.
+#' @param kpi_df A KPI table, e.g. as created by \code{\link{get_kpi}}.
+#'
+#' @export
+values_legend <- function(values_df, kpi_df) {
+  kpis <- unique(values_df$kpi)
+  desc <- kpi_df %>%
+    dplyr::select(id, title) %>%
+    dplyr::filter(id %in% kpis)
+
+  paste(glue::glue_data(desc, "{id}: {title}"), collapse = "\n")
 }
