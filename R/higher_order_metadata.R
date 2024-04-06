dc03be989e2621f5ab8880985ae05113a5466d23
LCHansson <love.hansson@gmail.com>
2024-04-06 22:27:00 +0200

    Release fixes to internal higher order function

--- R/higher_order_metadata.R
+++ R/higher_order_metadata.R
@@ -5,17 +5,20 @@
 #' @return a Kolada KPI metadata table
 
 generate_kpi_metadata <- function(kpi_df) {
+  descvar <- "description"
+  oavar <- "operating_area"
+
   kpi_df |>
     dplyr::mutate(
-      estimated_source = description |>
+      estimated_source = {{ descvar }} |>
         stringr::str_extract("(?<=K\u00e4lla).+$") |>
         stringr::str_remove("^:") |>
         stringr::str_remove("\\.$") |>
         stringr::str_squish(),
       estimated_category = dplyr::case_when(
-        stringr::str_detect(operating_area, "[Ss]skol|[Gg]ymnasi|[Uu]tbild") ~ "Education",
-        stringr::str_detect(operating_area, "[Ss]jukv\u00e5rd|[Hh]äls") ~ "Health",
-        stringr::str_detect(operating_area, "[Oo]msorg|[Vv]\u00e4rd") ~ "Care",
+        stringr::str_detect({{ oavar }}, "[Ss]skol|[Gg]ymnasi|[Uu]tbild") ~ "Education",
+        stringr::str_detect({{ oavar }}, "[Ss]jukv\u00e5rd|[Hh]\u00e4ls") ~ "Health",
+        stringr::str_detect({{ oavar }}, "[Oo]msorg|[Vv]\u00e5rd") ~ "Care",
         TRUE ~ "Unknown"
       )
     )
