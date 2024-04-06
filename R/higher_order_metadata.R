dad74e00b2e6a4bf85d319ba314b5ba45794a324
LCHansson <love.hansson@gmail.com>
2024-04-06 20:00:00 +0200

    Explicate external package usage some more

--- R/higher_order_metadata.R
+++ R/higher_order_metadata.R
@@ -1,19 +1,21 @@
 #' Generate KPI metadata from a Kolada KPI object
 #'
+#' @param kpi_df A Kolada KPI tibble
+#'
 #' @return a Kolada KPI metadata table
 
 generate_kpi_metadata <- function(kpi_df) {
   kpi_df |>
-    mutate(
+    dplyr::mutate(
       estimated_source = description |>
-        str_extract("(?<=Källa).+$") |>
-        str_remove("^:") |>
-        str_remove("\\.$") |>
-        str_squish(),
-      estimated_category = case_when(
-        str_detect(operating_area, "[Ss]skol|[Gg]ymnasi|[Uu]tbild") ~ "Education",
-        str_detect(operating_area, "[Ss]jukvård|[Hh]äls") ~ "Health",
-        str_detect(operating_area, "[Oo]msorg|[Vv]ård") ~ "Care",
+        stringr::str_extract("(?<=K\u00e4lla).+$") |>
+        stringr::str_remove("^:") |>
+        stringr::str_remove("\\.$") |>
+        stringr::str_squish(),
+      estimated_category = dplyr::case_when(
+        stringr::str_detect(operating_area, "[Ss]skol|[Gg]ymnasi|[Uu]tbild") ~ "Education",
+        stringr::str_detect(operating_area, "[Ss]jukv\u00e5rd|[Hh]äls") ~ "Health",
+        stringr::str_detect(operating_area, "[Oo]msorg|[Vv]\u00e4rd") ~ "Care",
         TRUE ~ "Unknown"
       )
     )
